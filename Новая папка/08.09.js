34c6fc18027a5c17e6ab9ab7e92f5e046acf1831
VZ <vitali.zaikov@gmail.com>
2020-09-10 08:56:24 +0300

    Работает канвас

---
+++ Новая папка/08.09.js
@@ -0,0 +1,61 @@
+// var x = 150;
+// var y = 100;
+
+
+// if (x == 150) {
+// 	document.write(x + y);
+// 	}	 
+// else {
+// 		document.write(x - y);
+
+// }
+
+// switch () {
+// 	case 1:
+// 	document.write("Эстония");
+// 	break;
+// 	case 2:
+// 	document.write("Европа");
+// 	break;
+// 	case 3:
+// 	document.write("Россия");
+// 	break;
+// 	default:
+// 	document.write("Другая страна")
+// }
+
+
+// var i = 0;
+// for (; i < 20; ) {
+// 	document.write(i) ;
+// 	i++;
+// }
+
+
+// var x = 0;
+// for (; x <= 20 ; x += 2){
+// 	document.write(x + "<br />")
+// }
+
+// var sum = 0;
+// for (i = 4; i<8; i++){
+// 	if (i == 6) {
+// 		continue;
+// 	}
+// 	sum += i;
+// }
+// document.write(sum);
+
+
+function name(named) {
+	alert("hi " + named);
+}
+name("ilja");
+name("ilja");
+name("ilja");
+
+
+
+// function MyFunction(para1, para2, para3) {
+	
+// }
---
+++ Новая папка/js.js
@@ -0,0 +1,74 @@
+// var x = 150;
+// var y = 100;
+
+
+// if (x == 150) {
+// 	document.write(x + y);
+// 	}	 
+// else {
+// 		document.write(x - y);
+
+// }
+
+// switch () {
+// 	case 1:
+// 	document.write("Эстония");
+// 	break;
+// 	case 2:
+// 	document.write("Европа");
+// 	break;
+// 	case 3:
+// 	document.write("Россия");
+// 	break;
+// 	default:
+// 	document.write("Другая страна")
+// }
+
+
+// var i = 0;
+// for (; i < 20; ) {
+// 	document.write(i) ;
+// 	i++;
+// }
+
+
+// var x = 0;
+// for (; x <= 20 ; x += 2){
+// 	document.write(x + "<br />")
+// }
+
+// var sum = 0;
+// for (i = 4; i<8; i++){
+// 	if (i == 6) {
+// 		continue;
+// 	}
+// 	sum += i;
+// }
+// document.write(sum);
+
+
+// function name(named) {
+// 	alert("hi " + named);
+// }
+// name("ilja");
+// name("ilja");
+// name("ilja");
+
+
+
+// function MyFunction(para1, para2, para3) {
+	
+// }
+
+
+
+// function Myfunction(name, age){
+
+
+// }
+
+		var c=document.getElementById("canvas1");
+		var ctx=c.getContext("2d");
+		ctx.fillStyle ="rgba(0, 200, 0, 1)";
+		ctx.fillRect(20, 20, 100, 100);
+
